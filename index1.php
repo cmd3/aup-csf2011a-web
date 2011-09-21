24805e4beddc42fcf1249780a4cbc2552525ae38
christian desderio <cmd3@csserver2.(none)>
2011-09-21 14:05:47 +0800

    exercise_theme

--- index1.php
+++
@@ -1,5 +0,0 @@
-				Date: <?echo date("M d, Y")?>
-				<h1>My Web Page</h1>
-				<p class="para">Hi everyone!!<br /> I'm Christian M. Desiderio and i'm 16 years old for real..xD<br />I'm studying here in AUP and I'm taking BSCS.<br />I can play guitar but only the basics and I also love to use computers.</p>
-				<img src="http://upload.wikimedia.org/wikipedia/en/2/2d/Angry_Birds_promo_art.png" />
-
